35f4e3c6c6a114efdea567a6c76c3d6f6de3c27a
mornflakes928 <77254405+mornflakes928@users.noreply.github.com>
2021-01-23 13:53:54 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -23,7 +23,7 @@ class MirrorStatus:
  
  
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['▓', '▓', '▓', '▓', '▓', '▓', '▓']
+PROGRESS_INCOMPLETE = ['▰', '▰', '▰', '▰', '▰', '▰', '▰']
  
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
  
@@ -80,17 +80,17 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '▓' * cFull
+    p_str = '▰' * cFull
     if cPart >= 0:
         p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += '░' * (PROGRESS_MAX_SIZE - cFull)
+    p_str += '▱' * (PROGRESS_MAX_SIZE - cFull)
     p_str = f"[{p_str}]"
     return p_str
  
  
 def get_readable_message():
     with download_dict_lock:
-        msg = "<b>✥════ ID ════✥</b>"
+        msg = "<b>✥══ @DWIKIKK ══✥</b>"
         for download in list(download_dict.values()):
             msg += f"\n📁 𝗙𝗶𝗹𝗲𝗡𝗮𝗺𝗲: <code>{download.name()}</code>"
             msg += f"\n {download.status()}"
