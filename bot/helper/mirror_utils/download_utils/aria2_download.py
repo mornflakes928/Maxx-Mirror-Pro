4db4cd88a4ca36ea456ec1ebbe37737d67a5e7eb
mornflakes928 <77254405+mornflakes928@users.noreply.github.com>
2021-01-23 14:11:43 +0700

    Update aria2_download.py

--- bot/helper/mirror_utils/download_utils/aria2_download.py
+++ bot/helper/mirror_utils/download_utils/aria2_download.py
@@ -31,7 +31,7 @@ class AriaDownloadHelper(DownloadHelper):
             gdrive = GoogleDriveHelper(None)
             smsg, button = gdrive.drive_list(sname)
           if smsg:
-              dl.getListener().onDownloadError(f'😡😡File is already available in drive. You should have search before mirror any file. You might get ban if you do this again. This download has been stopped.\n\n')
+              dl.getListener().onDownloadError(f'⚠️⚠️ File sudah ada di database, harap /list dahulu sebelum menggunakan /mirror, anda akan dikick jika melakukan ini lagi. This download has been stopped.\n\n')
               sendMarkup(" Here are the search results:👇👇", dl.getListener().bot, dl.getListener().update, button)
               aria2.remove([download])
           return
